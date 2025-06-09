6434f8831cd1d6eef61d1e86dfd3f4503db67ba0
Kaleb Seifert <seifertkaleb@gmail.com>
2025-06-09 10:39:03 -0400

    Added report and sys args

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from stats import count_words, letters, sort_dict
+import sys
 
 def get_book_text(file):
-    with open(file) as f:
-        book = f.read()
-    return book
+    try:
+        with open(file) as f:
+            book = f.read()
+        return book
+    except:
+        print("Warning file does not exist")
+        sys.exit(2)
 
 def report(sorted_list):
     print(
@@ -20,11 +25,13 @@ def report(sorted_list):
     print("============= END ===============")
 
 def main():
-    contents = get_book_text('books/frankenstein.txt')
+    contents = get_book_text(sys.argv[1])
     total_letters = letters(contents)
     sort = sort_dict(total_letters)
     report(sort)
 
-    
+if len(sys.argv) != 2:
+    print("Please make sure to have 2 argument: 'main.py' and 'Path to file you want read'")
+    sys.exit(1)
 
 main()
